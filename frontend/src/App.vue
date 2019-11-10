201e274a1bbe56d87280ccdaa544b2c9ea5a1328
mitch <robertsmj1138@gmail.com>
2019-11-10 09:17:15 -0500

    add bulma

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <section class="section">
+      <div class="container">
+        <h1 class="title">Hello World</h1>
+        <p class="subtitle">
+          we did it
+        </p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -9,7 +18,9 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '~bulma/bulma.sass';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
